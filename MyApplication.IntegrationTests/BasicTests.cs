7d615b9c048abf2ad4105642bd9edd373f1ca58b
InDieTasten <indietasten@gmail.com>
2019-10-11 04:41:45 +0200

    Add basic integration tests

---
+++ MyApplication.IntegrationTests/BasicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyApplication.IntegrationTests
+{
+    public class BasicTests
+    : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home/Index")]
+        [InlineData("/Home/Privacy")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
