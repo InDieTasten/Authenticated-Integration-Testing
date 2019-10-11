5d88b09c6413edcde010b70dde1b123c97d9d4d9
InDieTasten <indietasten@gmail.com>
2019-10-11 05:20:27 +0200

    Add InMemoryDb application factory

---
+++ MyApplication.IntegrationTests/AuthenticationTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyApplication.Data;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyApplication.IntegrationTests
+{
+    public class AuthenticationTests
+    : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public AuthenticationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_SecurePageRequiresAnAuthenticatedUser()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync("/Home/Secure");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/Identity/Account/Login",
+                response.Headers.Location.OriginalString);
+        }
+    }
+
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<ApplicationDbContext>((options, context) =>
+                {
+                    context.UseInMemoryDatabase("InMemoryDbForTesting")
+                        .UseInternalServiceProvider(serviceProvider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    var userManager = scopedServices.GetRequiredService<UserManager<IdentityUser>>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed the database with test data.
+                        InitializeUserStore(userManager).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                            "database with test messages. Error: {Message}", ex.Message);
+                    }
+                }
+            });
+        }
+
+        private async Task InitializeUserStore(UserManager<IdentityUser> userManager)
+        {
+            var alice = new IdentityUser
+            {
+                Email = "alice@example.org"
+            };
+            await userManager.CreateAsync(alice, "#SecurePassword123");
+            var confirmationToken = await userManager.GenerateEmailConfirmationTokenAsync(alice);
+            await userManager.ConfirmEmailAsync(alice, confirmationToken);
+        }
+    }
+}
